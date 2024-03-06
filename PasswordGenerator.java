d698dda2ac69b7a75d9e2dfed251b0f2124cc8d8
Shaik-uzaif <161204554+Shaik-uzaif@users.noreply.github.com>
2024-03-06 06:51:43 +0530

    Add files via upload
    java.task #RK technologies

---
+++ PasswordGenerator.java
@@ -0,0 +1,22 @@
+// TASK: PASSWORD GENERATOR
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String num = "0123456789";
+        String specialChars = "<>,.?/}]{]+_-) (@#$%^&*1";
+        String combination = upper + lower + specialChars + num;
+
+        int len = 10;
+        char[] password = new char[len];
+        Random r = new Random();
+        for (int i = 0; i < len; i++) {
+            password[i] = combination.charAt(r.nextInt(combination.length()));
+        }
+        System.out.println("Generate Password is:" + new String(password));
+    }
+
+}
